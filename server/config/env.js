aae3a67417d917be9d7d465f4cb5fde2034107af
Samuel Diaz Licht <samuelrodolfolicht@gmail.com>
2026-06-06 18:04:26 -0500

    feat(config): add environment variables and MongoDB connection setup

---
+++ server/config/env.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+// Cargar variables de entorno
+dotenv.config({ path: './server/.env' });
+
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+};
